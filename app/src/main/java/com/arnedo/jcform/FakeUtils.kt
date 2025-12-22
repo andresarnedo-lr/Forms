f70f7b226c6af6ac39e7dae57184a83e4ae8c093
Andres Arnedo <alfredoandresarnedo@gmail.com>
2025-12-22 20:18:50 +0100

    feat: nueva funcion similar pero con mayor prolongacion de delayTime

--- app/src/main/java/com/arnedo/jcform/FakeUtils.kt
+++ app/src/main/java/com/arnedo/jcform/FakeUtils.kt
@@ -6,3 +6,8 @@ import kotlin.random.Random
 suspend fun simulateDelay() {
     delay(Random.nextLong(300, 1_500))
 }
+
+suspend fun simulateDelayLong() {
+    delay(Random.nextLong(3_000, 5_000))
+}
+
