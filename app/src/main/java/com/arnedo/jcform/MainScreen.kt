1077075a139c7377ad1a75781a677a69ba9f4ba7
Andres Arnedo <alfredoandresarnedo@gmail.com>
2025-12-24 14:47:53 +0100

    feat(ui): Añadir title del formulario y TextField Nombre

--- app/src/main/java/com/arnedo/jcform/MainScreen.kt
+++ app/src/main/java/com/arnedo/jcform/MainScreen.kt
@@ -7,16 +7,29 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.arnedo.jcform.ui.theme.JCFormTheme
+import com.arnedo.jcform.ui.theme.Typography
 
 @Preview(showBackground = true)
 @Composable
-fun MainPreview(){
+fun MainPreview() {
     JCFormTheme {
         MainView(Modifier.padding(top = dimensionResource(R.dimen.common_padding_middle)))
     }
@@ -25,15 +38,36 @@ fun MainPreview(){
 
 @Composable
 fun MainView(modifier: Modifier) {
-    Box(modifier.fillMaxWidth()){
-        Column() { }
-    }
-    Box(Modifier
-        .fillMaxSize()
-        .background(colorResource(R.color.progress_background))
-        .clickable{}
-    ) {
+    var nameValue by remember { mutableStateOf("") }
+
+
+    Box(modifier.fillMaxWidth()) {
+        Column(Modifier.padding(horizontal = dimensionResource(R.dimen.common_padding_default))) {
+            Text(
+                stringResource(R.string.form_title),
+                style = Typography.titleLarge
+            )
+        }
+        OutlinedTextField(
+            value = nameValue,
+            onValueChange = { nameValue = it},
+            modifier = Modifier.fillMaxWidth(),
+            label =  {
+                Text(stringResource(R.string.hint_name))
+            },
+            leadingIcon = {
+                Icon(Icons.Default.Person, contentDescription = null)
+            })
+            }
+
+
+                    Box (Modifier
+                        .fillMaxSize()
+                        .background(colorResource(R.color.progress_background))
+                        .clickable {},
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
 
     }
-
-}
