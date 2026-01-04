f189dfa2b4943287bc04f168e84858c7a025c5a8
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-01-04 18:25:23 +0100

    feat(UI): inicio de validacion de errores al presionar save

--- app/src/main/java/com/arnedo/jcform/FormUtils.kt
+++ app/src/main/java/com/arnedo/jcform/FormUtils.kt
@@ -1,5 +1,6 @@
 package com.arnedo.jcform
 
+import android.content.Context
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -17,4 +18,15 @@ fun convertMillisToDate(millis : Long) : String {
         ZoneId.of("UTC")
     ).format(dateFormatter)
 
+}
+
+fun foundErrors(context : Context,
+                name : String,
+                surname : String,
+                height : String): String?  {
+    return if (name.isBlank() || surname.isBlank() || (height.toIntOrNull() ?: 0) < context.resources.getInteger(R.integer.height_min_value)){
+        context.getString(R.string.form_invalid)
+    }else {
+        return  null
+    }
 }
--- app/src/main/java/com/arnedo/jcform/MainScreen.kt
+++ app/src/main/java/com/arnedo/jcform/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.arnedo.jcform
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -23,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.isDebugInspectorInfoEnabled
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.integerResource
@@ -66,6 +68,8 @@ fun MainView(
 
     val profiles = listOf("Estudiante", "Programador")
     val (selectedOption, onOptionSelected) = remember { mutableStateOf(profiles[0]) }
+    val context = LocalContext.current
+
 
     if (isClean) {
         dateValue = null
@@ -188,13 +192,19 @@ fun MainView(
             //Save
             Button(
                 onClick = {
-                    val user = User(nameValue,
-                        surnameValue,
-                        heightValue.toInt(),
-                        dateValue ?: 0,
-                        selectedOption,
-                        notesValue)
-                    onSave(user)
+                    val errors = foundErrors(context, nameValue, surnameValue, heightValue)
+                    if(errors == null){
+                        val user = User(nameValue,
+                            surnameValue,
+                            heightValue.toInt(),
+                            dateValue ?: 0,
+                            selectedOption,
+                            notesValue)
+                        onSave(user)
+                    }else {
+                        Log.e("CursosANT", "MainView: $errors")
+                    }
+
                 },
                 modifier = Modifier
                     .fillMaxWidth()
